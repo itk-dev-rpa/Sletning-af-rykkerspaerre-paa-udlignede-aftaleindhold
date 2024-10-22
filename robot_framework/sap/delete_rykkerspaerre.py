8197b1fc2e8c509a2c8a4baa869d71ec317f3cc2
Mathias G <ghbm@aarhus.dk>
2024-10-22 11:12:36 +0200

    Fixed edge case

--- robot_framework/sap/delete_rykkerspaerre.py
+++ robot_framework/sap/delete_rykkerspaerre.py
@@ -45,8 +45,20 @@ def delete_rykkerspaerre(session, orchestrator_connection: OrchestratorConnectio
     aftale_tree.nodeContextMenu(node_key)
     aftale_tree.selectContextMenuItemByText("Ændr aftaleindhold")
 
-    # Go to 'Betalingsdata' and press 'Opret yderl. spærre'
+    # Go to 'Betalingsdata'
     session.findById("wnd[0]/usr/subBDT_AREA:SAPLBUSS:0021/tabsBDT_TABSTRIP01/tabpBUSCR02_01").select()
+
+    # Check if rykkerspærre should be skipped
+    rs_type = session.findById("wnd[0]/usr/subBDT_AREA:SAPLBUSS:0021/tabsBDT_TABSTRIP01/tabpBUSCR02_01/ssubGENSUB:SAPLBUSS:0029/ssubGENSUB:SAPLBUSS:7135/subA04P02:SAPLFMCA_PSOB_BDT2:0330/ctxtSPSOB_SCR_2110_H3-DUNN_REASON").text
+    rs_date = session.findById("wnd[0]/usr/subBDT_AREA:SAPLBUSS:0021/tabsBDT_TABSTRIP01/tabpBUSCR02_01/ssubGENSUB:SAPLBUSS:0029/ssubGENSUB:SAPLBUSS:7135/subA04P03:SAPLZDKD0001_CUSTOM_SCREENS:0510/ctxtGV_DUNN_TDATE_CO").text
+
+    if rs_type == 'H' and rs_date == '31.12.2999':
+        # Skip and go back
+        session.findById("wnd[0]/tbar[0]/btn[3]").press()
+        orchestrator_connection.set_queue_element_status(queue_element.id, QueueStatus.DONE, message="Sprunget over: H 31.12.2999")
+        return
+
+    # Press 'Opret yderl. spærre'
     session.findById("wnd[0]/usr/subBDT_AREA:SAPLBUSS:0021/tabsBDT_TABSTRIP01/tabpBUSCR02_01/ssubGENSUB:SAPLBUSS:0029/ssubGENSUB:SAPLBUSS:7135/subA04P02:SAPLFMCA_PSOB_BDT2:0330/btnPUSH_DUNN_LOCK").press()
 
     # Mark all and delete
